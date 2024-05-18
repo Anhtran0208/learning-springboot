fcce2feb29c88f8c29265f3f6fb26c5d2e624c91
ANHTRAN <tran36a@mtholyoke.edu>
2024-05-18 16:40:27 -0400

    review string

---
+++ Core-Java/String1.java
@@ -0,0 +1,12 @@
+
+public class String1 {
+    public static void main (String a[]){
+        String name = "Kevin";
+        System.out.println(name.charAt(0));
+
+        StringBuffer sb = new StringBuffer("Navin");
+        sb.append(" Reddy");
+        System.out.println(sb);
+
+    }
+}
